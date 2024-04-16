108d6bbc7b646c9ea911dff2516ed36988218544
muhammed harif <harifn786@gmail.com>
2024-04-16 10:39:38 +0530

    basic structure is completed

--- blog/views.py
+++ blog/views.py
@@ -24,7 +24,6 @@ class BlogCategoryView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
-        print("@@@@@@@@@@@@@@@@@@@@@@@",context)
         return context
 
 class BlogTagView(ListView):
